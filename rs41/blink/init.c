353241e9c19520de9dcb37247953d33b97e215e5
Tomáš Dubina <t.dubina@volny.cz>
2021-03-02 16:18:07 +0100

    Add RS41 blink demo project

---
+++ rs41/blink/init.c
@@ -0,0 +1,40 @@
+// Global LibOpenCM3 libraries
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+// STM32F100 libraries
+#include <libopencm3/stm32/f1/rcc.h>
+#include <libopencm3/stm32/f1/gpio.h>
+// Another libraries
+#include <stdint.h>
+#include "init.h"
+
+void gpio_setup(void) {
+    /* Enable the correct clock. */
+    /* Enable GPIO RCC clock. */
+    rcc_periph_clock_enable(LED_GREEN_RCC);
+    rcc_periph_clock_enable(LED_RED_RCC);
+
+    /* Set LED_PIN to 'output push-pull'. */
+    gpio_set_mode(LED_GREEN_GPIO, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, LED_GREEN_PIN);
+    gpio_set_mode(LED_RED_GPIO, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, LED_RED_PIN);
+}
+
+void clock_setup(void) {
+    /* hse24, pll to 24 */
+    /* 1/24 MHz = perioda 41.6666 ns */
+    const struct rcc_clock_scale clock = {
+        .pll_mul = RCC_CFGR_PLLMUL_PLL_CLK_MUL2,    // Nasob zdroj 2 krat
+        .pll_source = RCC_CFGR_PLLSRC_HSE_CLK,      // Zdrojem je HSE 24 MHz (krystal)
+        .hpre = RCC_CFGR_HPRE_DIV2,                 // Hlavni delicka za PLL 48/2 = 24 MHz
+        .adcpre = RCC_CFGR_ADCPRE_DIV2,             // Delicka k ADC, 24/2 = 12 MHz
+        .ppre1 = RCC_CFGR_PPRE_NODIV,               // Delicka APB1 (max 24 MHz), 24/1 = 24 MHz
+        .ppre2 = RCC_CFGR_PPRE_NODIV,               // Delicka APB2 (max 24 MHz), 24/1 = 24 MHz
+        .flash_waitstates = 0x00,                   // 0WS from 0-24MHz, 1WS from 24-48MHz, 2WS from 48-72MHz
+        .prediv1 = RCC_CFGR2_PREDIV_NODIV,          // Delicka pred PLL od HSE krystalu, 24/1 = 24 MHz
+        .ahb_frequency  = 24000000,  // Vysledne frekvence za AHB
+        .apb1_frequency = 24000000,  // Vysledne frekvence za APB1
+        .apb2_frequency = 24000000,  // Vysledne frekvence za APB2
+    };
+
+    rcc_clock_setup_pll(&clock);
+}
