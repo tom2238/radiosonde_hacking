e5a56c1c3bf6e650984d9c636a302020be445bbf
Tomáš Dubina <t.dubina@volny.cz>
2021-04-10 20:11:29 +0200

    Licence added into file

--- rs41/si4032_cw/utils.c
+++ rs41/si4032_cw/utils.c
@@ -176,6 +176,21 @@ extern char* ultoa( unsigned long value, char *string, int radix )
 #endif // __cplusplus
 
 /*
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
  * console_putc(char c)
  *
  * Send the character 'c' to the USART, wait for the USART
--- rs41/si4032_fifo/utils.h
+++ rs41/si4032_fifo/utils.h
@@ -1,3 +1,21 @@
+/*
+  Copyright (c) 2011 Arduino.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 #ifndef UTILS_H
 #define UTILS_H
 
@@ -20,6 +38,21 @@ extern char* ultoa( unsigned long value, char *string, int radix ) ;
 #ifdef __cplusplus
 } // extern "C"
 #endif // __cplusplus
+
+/*
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 // Define USART console, use USART3 for Vaisala RS41
 #define CONSOLE_UART USART3
